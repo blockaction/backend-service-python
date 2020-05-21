4826a7278dd184ca8d2690e7582f58ae9e5bf615
parishilan <parishilanrayamajhi@gmail.com>
2020-05-21 12:23:28 +0545

    new instance ip added

--- client/client_conf.py
+++ client/client_conf.py
@@ -1,4 +1,4 @@
 def server_address():
-    host = '35.153.104.5'
+    host = '35.171.88.120'
     port = '4000'
     return '{}:{}'.format(host,port)
--- flask_app/common.py
+++ flask_app/common.py
@@ -18,7 +18,7 @@ def get_config():
 
 def api():
     # base_url = "https://api.prylabs.net"
-    base_url = 'http://35.153.104.5:4001'
+    base_url = 'http://35.171.88.120:4001'
     return base_url
 
 
